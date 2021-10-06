678e0dd61699b7862d45933648db3f9e33800e7b
Anibal <androso421@gmail.com>
2021-10-06 13:08:06 -0600

    fixing paths

--- quiz-app-v2/js/app.js
+++ quiz-app-v2/js/app.js
@@ -3,6 +3,6 @@ import { renderCustomizeMenu } from "./modules/customize.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     renderCustomizeMenu();
-    
+    console.log("working?")
 });
 
