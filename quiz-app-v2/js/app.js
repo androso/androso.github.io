9ffa6b1c622912aecf06f8cd16e1f9ccc978e1b0
Anibal <androso421@gmail.com>
2021-10-06 13:43:59 -0600

    fixing files paths

--- quiz-app-v2/js/app.js
+++ quiz-app-v2/js/app.js
@@ -1,10 +1,10 @@
 // import { renderCustomizeMenu } from "/modules/customize.js"; 
 console.log("should be working");
-import renderCustomizeMenu from "/quiz-app/js/modules/Customize.js";
+// import renderCustomizeMenu from "/quiz-app/js/modules/Customize.js";
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("working?")
-    renderCustomizeMenu();
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("working?")
+//     renderCustomizeMenu();
+// });
 
